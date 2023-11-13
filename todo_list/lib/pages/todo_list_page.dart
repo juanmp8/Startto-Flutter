60cddf2782b4b6f3429dd0c06cacd6c46303b5bc
Juan <juanmendespeixoto@gmail.com>
2023-11-13 11:12:12 -0300

    text field e limpar tudo

--- todo_list/lib/pages/todo_list_page.dart
+++ todo_list/lib/pages/todo_list_page.dart
@@ -1,34 +1,67 @@
 import 'package:flutter/material.dart';
 
 class TodoListPage extends StatelessWidget {
-  const TodoListPage({super.key});
+  TodoListPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Center(
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 16),
-        child: TextField(
-          decoration: InputDecoration(
-            labelText: 'Preço',
-            hintText: 'exemplo@email.com',
-            //border: OutlineInputBorder(),
-            errorText: null,
-            prefixText: 'R\$',
-            suffixText: 'cm',
-            labelStyle: TextStyle(
-              fontSize: 40,
-            ),
-          ),
-          obscureText: false,
-          keyboardType: TextInputType.number,
-          style: TextStyle(
-            fontWeight: FontWeight.w700,
-            color: Colors.purple,
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Row(
+                children: [
+                  Expanded(
+                    flex: 2,
+                    child: TextField(
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Adicione uma Tarefa',
+                        hintText: 'Ex. Ler um livro',
+                      ),
+                    ),
+                  ),
+                  SizedBox(width: 8),
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: Icon(
+                      Icons.add,
+                      size: 30,
+                    ),
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Color(0xff00d7f3),
+                      padding: EdgeInsets.all(14),
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 16),
+              Row(
+                children: [
+                  Expanded(
+                    flex: 2,
+                    child: Text('Você possui X tarefas pendentes'),
+                  ),
+                  SizedBox(width: 8),
+                  Expanded(
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      child: Text('Limpar Tudo'),
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Color(0xff00d7f3),
+                        padding: EdgeInsets.all(14),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
           ),
         ),
       ),
-    ));
+    );
   }
 }
